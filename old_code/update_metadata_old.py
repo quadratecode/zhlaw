fe408cf06b16fa525fe7e852c113174e2920b204
quadratecode <quadratecode@piccolaw.ch>
2025-07-04 21:13:14 +0200

    refactor: Remove obsolete scripts for metadata processing and file management

--- old_code/update_metadata_old.py
+++
@@ -1,5 +0,0 @@
-from src.modules.zhlex_module import update_metadata
-
-# Update source metadata for all laws
-processed_data = "data/zhlex/zhlex_data/zhlex_data_processed.json"
-update_metadata.main("data/zhlex/zhlex_files", processed_data)
